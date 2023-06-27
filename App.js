9c34ef91bfc9116cd98336916cc38a7e9eb8b315
nikkuts <nickkuts888@gmail.com>
2023-06-27 12:57:55 +0300

    up

--- App.js
+++ App.js
@@ -14,8 +14,8 @@ export default function App() {
 
   return (
     <>
-      {/* <RegistrationScreen/> */}
-      <LoginScreen/>
+      <RegistrationScreen/>
+      {/* <LoginScreen/> */}
     </>
   );
 };
